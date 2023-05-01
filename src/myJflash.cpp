29b17cb2e15fc72078a5410371acea991129993c
guyd <guydvir2@gmail.com>
2023-05-01 22:42:37 +0300

    fix

---
+++ src/myJflash.cpp
@@ -0,0 +1,94 @@
+#include "myJflash.h"
+
+myJflash::myJflash(bool use_s)
+{
+	useSerial = use_s;
+}
+myJflash::~myJflash()
+{
+	_endFS();
+}
+void myJflash::set_filename(const char *filename)
+{
+	_filename = filename;
+}
+bool myJflash::readFile(JsonDocument &DOC, const char *filename)
+{
+	_startFS();
+	File readFile = LITFS.open(_retFilename(filename), "r");
+	DeserializationError error = deserializeJson(DOC, readFile);
+	readFile.close();
+
+	PRNT(F("\n>>> "));
+	PRNT(_retFilename(filename));
+
+	if (error)
+	{
+		PRNTL(F(":Read [failed]"));
+		PRNT(F("error:"));
+		PRNTL(error.c_str());
+		Serial.flush();
+		return 0;
+	}
+	else
+	{
+		PRNTL(F(":Read [OK]"));
+		Serial.flush();
+		return 1;
+	}
+}
+String myJflash::readFile2String(const char *fileName)
+{
+	_startFS();
+	File file = LITFS.open(_retFilename(fileName), "r");
+
+	if (file)
+	{
+		String t = file.readString();
+		file.close();
+		return t;
+	}
+	else
+	{
+		file.close();
+		return "";
+	}
+}
+bool myJflash::writeFile(JsonDocument &DOC, const char *filename)
+{
+	return _saveFile(DOC, filename);
+}
+void myJflash::_startFS()
+{
+#if defined(ESP8266)
+	LITFS.begin();
+#elif defined(ESP32)
+	LITFS.begin(true);
+#endif
+}
+void myJflash::_endFS()
+{
+	LITFS.end();
+}
+const char *myJflash::_retFilename(const char *f)
+{
+	if (f != nullptr)
+	{
+		return f;
+	}
+	else
+	{
+		return _filename;
+	}
+}
+bool myJflash::_saveFile(JsonDocument &DOC, const char *filename)
+{
+	File writefile = LITFS.open(_retFilename(filename), "w");
+	if (!writefile || (serializeJson(DOC, writefile) == 0))
+	{
+		writefile.close();
+		return 0;
+	}
+	writefile.close();
+	return 1;
+}
---
+++ src/myJflash.h
@@ -0,0 +1,46 @@
+#ifndef myjFlash_h
+#define myjFlash_h
+
+#include <FS.h>
+#include <LittleFS.h>
+#include <ArduinoJson.h>
+
+#define LITFS LittleFS
+
+#ifndef PRNT
+#define PRNT(a)    \
+    if (useSerial) \
+    Serial.print(a)
+#endif
+#ifndef PRNTL
+#define PRNTL(a)   \
+    if (useSerial) \
+    Serial.println(a)
+#endif
+
+class myJflash
+{
+
+protected:
+    const char *ver = "jFlash_v0.1";
+
+public:
+    bool useSerial = false;
+
+    myJflash(bool use_s = false);
+    ~myJflash();
+    void set_filename(const char *filename);
+    bool readFile(JsonDocument &DOC, const char *filename = nullptr);
+    bool writeFile(JsonDocument &DOC, const char *filename = nullptr);
+    // bool updateValue(JsonDocument &DOC, const char *filename = nullptr);
+    String readFile2String(const char *fileName = nullptr);
+
+private:
+    const char *_filename;
+
+    void _endFS();
+    void _startFS();
+    bool _saveFile(JsonDocument &DOC, const char *filename = nullptr);
+    const char *_retFilename(const char *f);
+};
+#endif
